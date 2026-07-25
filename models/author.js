1e0d65d77ade4a41fda9a92105c2219b9023bbe7
Tristan Cimaglia <tcimaglia20@gmail.com>
2026-07-24 19:42:38 -0700

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,20 @@ const authorSchema = new mongoose.Schema({
         required: true
     }
 })
+
+authorSchema.pre('deleteOne', { document: true, query: false }, async function() {
+    try {
+        if (!this._id) return;
+
+        const bookCount = await Book.countDocuments({ author: this._id });
+        
+        if (bookCount > 0) {
+            throw new Error('This author still has books');
+        }
+        
+    } catch {
+        next()
+    }
+});
 
 module.exports = mongoose.model('Author', authorSchema)
